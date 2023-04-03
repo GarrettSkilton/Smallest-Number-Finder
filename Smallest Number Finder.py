529eb4d1e420d7cb4e99245ff2ea6a1b26b56a0c
GarrettSkilton <128932204+GarrettSkilton@users.noreply.github.com>
2023-04-03 15:52:32 -0500

    Add files via upload

---
+++ Smallest Number Finder.py
@@ -0,0 +1,5 @@
+smallest_so_far = 300
+for the_num in [10, 15, 44, 75, 2, 155, 66, 47, 133, 127, 5, 3, 1] :
+    if the_num < smallest_so_far :
+        smallest_so_far = the_num
+print('The smallest number in the set is', smallest_so_far)
